e4bf997a491316d6cb839b82cb3d2be5c4ea6769
Nodragem <geoffrey.megardon@gmail.com>
2024-05-31 11:05:29 +0100

    refactoring enemy

---
+++ enemies/melee/enemy_entity.gd
@@ -0,0 +1,6 @@
+extends CharacterBody3D
+class_name EnemyEntity
+
+@onready var navigation_agent:NavigationAgent3D=$NavigationAgent3D
+@onready var model: MeleeSkin=$MeleeSkin
+@onready var controller: MeleeController=$MeleeController
