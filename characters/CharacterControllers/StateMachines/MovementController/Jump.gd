6d1973a7b607ced426f4f3ad386b8d763462c5a7
Nodragem <geoffrey.megardon@gmail.com>
2024-06-20 18:24:39 +0100

    add restart and quit options when gameover

--- characters/CharacterControllers/StateMachines/MovementController/Jump.gd
+++ characters/CharacterControllers/StateMachines/MovementController/Jump.gd
@@ -1,7 +1,6 @@
 extends PlayerState
 
-@export var jump_initial_impulse := 20.0
-@export var jump_additional_force := 4.5
+@export var jump_initial_impulse := 10.0
 
 func physics_process(delta: float) -> void:
 	_parent.physics_process(delta)
