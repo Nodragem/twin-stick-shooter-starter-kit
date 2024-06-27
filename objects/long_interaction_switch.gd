481af193b8849a0cbcaf326a3ac6b8b5005237af
Nodragem <geoffrey.megardon@gmail.com>
2024-06-27 20:53:28 +0100

    small progress

--- objects/long_interaction_switch.gd
+++ objects/long_interaction_switch.gd
@@ -3,20 +3,22 @@ class_name LongInteractable
 
 enum {OFF, PROGRESSING, REGRESSING, ON}
 var state = OFF
-var progress
+var progress: float = 0
 @export_range(0,100) var progression_rate:float=10
 @export_range(0,100) var regression_rate:float=10
+@onready var anim_tree: AnimationTree = $AnimationTree
 
 func _process(delta):
 	if state == PROGRESSING:
-		progress += progression_rate*delta
+		progress = min(100, progress + progression_rate*delta)
 	elif state == REGRESSING:
-		progress -= regression_rate*delta
-
+		progress = min(0, progress - regression_rate*delta)
+	anim_tree["parameters/time_seek/seek_request"] = progress/100.0
+	
 func on_interaction():
 	if state == OFF or state == REGRESSING:
 		state = PROGRESSING
-		print("Progessing")
+		print("Progressing")
 		$AnimationPlayer.play("switch_charging")
 
 func on_interruption():
