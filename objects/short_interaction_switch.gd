2989bab5bae811400eff4345196730bc48f7ff47
Nodragem <geoffrey.megardon@gmail.com>
2024-07-19 18:52:52 +0100

    make sure that the short switch shows the right state when game starts

--- objects/short_interaction_switch.gd
+++ objects/short_interaction_switch.gd
@@ -2,13 +2,16 @@ extends Switch
 class_name ShortSwitch
 
 func _ready():
-	on_interaction(true)
+	update_animation()
 
 func on_interaction(requested):
 	if requested == false: 
 		return
-	
 	is_activated = !is_activated
+	update_animation()
+
+
+func update_animation():
 	if is_activated:
 		print("interacting ON")
 		$AnimationPlayer.play("on_state")
